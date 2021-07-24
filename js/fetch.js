9855edd2edbcd3942ce3535dfb84a9b2825a2e15
seanmars <mars0222@gmail.com>
2021-07-24 22:28:44 +0800

    Add color palette to target element

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
-const Url =
-  "http://127.0.0.1:5000/palette/defd0f-886f84-defd0f-b43150-31b1d4?num=2";
+const BaseUrl = 'http://127.0.0.1:5000';
+const PaletteUrl = `${BaseUrl}/palette/defd0f-886f84-defd0f-b43150-31b1d4?num=2`;
 
 const createElement = (palette) => {
   const dataPaletteElements = document.querySelectorAll("div.dataPalette");
@@ -11,7 +11,33 @@ const createElement = (palette) => {
   }
 };
 
-fetch(Url, { credentials: "same-origin" })
+const createColorElement = (hex) => {
+  const block = document.createElement('div');
+  block.style.backgroundColor = `#${hex}`;
+  block.style.width = '20px';
+  block.style.height = '20px'
+  block.style.margin = '0.25rem';
+  return block;
+};
+
+const applyImages = (images) => {
+  if (!images) {
+    return;
+  }
+
+  const dataPaletteElements = document.querySelectorAll("div.dataPalette");
+  const appliedImages = images.slice(0, dataPaletteElements.length);
+  appliedImages.forEach(/** @param {{file_name: string, palette: string, url: string}} img */(img, idx) => {
+    const targetElement = dataPaletteElements[idx];
+    const colorElements = img.palette
+      .split('-')
+      .map(hex => createColorElement(hex));
+
+    colorElements.forEach(ele => targetElement.appendChild(ele));
+  });
+};
+
+fetch(PaletteUrl, { credentials: "same-origin" })
   .then((response) => response.json())
   .then((data) => {
     const {
@@ -23,22 +49,20 @@ fetch(Url, { credentials: "same-origin" })
 
     //deleteAllResult();
 
-    if (!images) {
-      return;
-    }
+    applyImages(images);
 
-    images.forEach((v, idx) => {
-      const distance = distances[idx];
-      const {
-        /** @type {string} file_name */
-        file_name,
-        /** @type {string} palette */
-        palette,
-      } = v;
+    // images.forEach((v, idx) => {
+    //   const distance = distances[idx];
+    //   const {
+    //     /** @type {string} file_name */
+    //     file_name,
+    //     /** @type {string} palette */
+    //     palette,
+    //   } = v;
 
-      // palette color
-      console.log(images.length);
-      createElement(palette);
-    });
+    //   // palette color
+    //   console.log(images.length);
+    //   createElement(palette);
+    // });
   })
   .catch((error) => console.error(error));
