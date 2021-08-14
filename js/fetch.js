d0b4a5cc148c550f8375c80247fb89c3a31078d5
wuianski <wuiyeh@gmail.com>
2021-08-14 22:36:29 +0800

    update dataset

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
 function postHex(joinB) {
   //console.log(joinB);
-  const BaseUrl = "https://data.youmayalsolike.ml/";
+  const BaseUrl = "https://data.youmayalsolike.ml/"; //http://127.0.0.1:5000  //https://data.youmayalsolike.ml/
   const cameraPalette = `${joinB}`;
   const PaletteUrl = `${BaseUrl}/palette/${cameraPalette}?num=ˇ`;
 
