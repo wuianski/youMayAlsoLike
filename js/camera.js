df0cf6a9af7eaa527ed9ee600a5ee500980bf449
wuianski <wuiyeh@gmail.com>
2023-05-13 16:59:27 +0800

    v2

--- js/camera.js
+++ js/camera.js
@@ -1,112 +1,108 @@
-    const PALETTE_COLORS_COUNT = 5;
-    const IMAGE_MAX_WIDTH = 400;
+const PALETTE_COLORS_COUNT = 5;
+const IMAGE_MAX_WIDTH = 400;
 
-    const paletteElement = document.querySelector(".palette");
-    const paletteElements = [];
-    let i = 0;
-    while (i < PALETTE_COLORS_COUNT) {
-      const li = document.createElement('li');
-      paletteElement.appendChild(li);
-      paletteElements.push(li);
-      i++;
-    }
+const paletteElement = document.querySelector(".palette");
+const paletteElements = [];
+let i = 0;
+while (i < PALETTE_COLORS_COUNT) {
+  const li = document.createElement("li");
+  paletteElement.appendChild(li);
+  paletteElements.push(li);
+  i++;
+}
 
-    const paletteExtractor = new PaletteExtractor();
+const paletteExtractor = new PaletteExtractor();
 
-    var video = document.getElementById("video");
-    var canvas = document.getElementById("canvas");
+var video = document.getElementById("video");
+var canvas = document.getElementById("canvas");
 
-    // var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
-    //document.body.appendChild(canvas);
+// var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+//document.body.appendChild(canvas);
 
-    /** Get access to the camera! **/
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      /** Not adding `{ audio: true }` since we only want video now **/
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then(function (stream) {
-          video.srcObject = stream;
-          video.play();
-        });
-    }
+/** Get access to the camera! **/
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  /** Not adding `{ audio: true }` since we only want video now **/
+  navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    video.srcObject = stream;
+    video.play();
+  });
+}
 
-    video.addEventListener(
-      "loadeddata",
-      function () {
-        // 等 the event loaddata finished
-        loadBodyPix();
-      },
-      false
+video.addEventListener(
+  "loadeddata",
+  function () {
+    // 等 the event loaddata finished
+    loadBodyPix();
+  },
+  false
+);
+
+/** Elements for taking the snapshot **/
+var canvas2 = document.getElementById("canvas2");
+var context = canvas2.getContext("2d");
+
+var tempState = false;
+
+/**  Trigger photo take by click mouse button on anywhere of screen **/
+function WhichButton(event) {
+  if (event.button == 0) {
+    tempState = true;
+    //ctx.drawImage(video, 0, 0, 640, 480);
+    context.drawImage(canvas, 0, 0, 640, 480);
+
+    const data = ctx.getImageData(0, 0, 640, 480).data;
+    /** Extracts the colors palette from image data. **/
+    const hexPalette = paletteExtractor.processImageData(
+      data,
+      PALETTE_COLORS_COUNT
     );
+    //console.log(hexPalette)
 
-  
+    /** combine 5 hex into a string for post to API (send to fetch.js) **/
+    let joinA = hexPalette.join("-");
+    let str = joinA.toString().split("#");
+    const joinB = str.join("");
+    postHex(joinB);
 
-    /** Elements for taking the snapshot **/
-    var canvas2 = document.getElementById("canvas2");
-    var context = canvas2.getContext("2d");
-
-    var tempState = false;
-
-    /**  Trigger photo take by click mouse button on anywhere of screen **/
-    function WhichButton(event) {
-      if (event.button == 0) {
-        tempState = true;
-        //ctx.drawImage(video, 0, 0, 640, 480);
-        context.drawImage(canvas, 0, 0, 640, 480);
-
-        const data = ctx.getImageData(0, 0, 640, 480).data;
-        /** Extracts the colors palette from image data. **/
-        const hexPalette = paletteExtractor.processImageData(
-          data,
-          PALETTE_COLORS_COUNT
-        );
-        //console.log(hexPalette)
-
-        /** combine 5 hex into a string for post to API (send to fetch.js) **/
-        let joinA = hexPalette.join("-");
-        let str = joinA.toString().split("#");
-        const joinB = str.join("");
-        postHex(joinB);
-
-        /** draw 5 hex on top of snapshot image **/
-        let index = 0;
-        for (const paletteColorElem of paletteElements) {
-          paletteColorElem.style.backgroundColor = hexPalette[index];
-          index++;
-        }
-      }
+    /** draw 5 hex on top of snapshot image **/
+    let index = 0;
+    for (const paletteColorElem of paletteElements) {
+      paletteColorElem.style.backgroundColor = hexPalette[index];
+      index++;
     }
+  }
+}
 
-    function loadBodyPix() {
-      options = {
-        multiplier: 0.75,
-        stride: 32,
-        quantBytes: 4,
-      };
-      bodyPix
-        .load(options)
-        .then((net) => perform(net))
-        .catch((err) => console.log(err));
-    }
+function loadBodyPix() {
+  options = {
+    multiplier: 0.75,
+    stride: 32,
+    quantBytes: 4,
+  };
+  bodyPix
+    .load(options)
+    .then((net) => perform(net))
+    .catch((err) => console.log(err));
+}
 
-    async function perform(net) {
-      while (1) {
-        const segmentation = await net.segmentPerson(video);
-        const coloredPartImage = bodyPix.toMask(segmentation);
-        const opacity = 1;
-        const flipHorizontal = false;
-        const maskBlurAmount = 0;
-        // Draw the mask image on top of the original image onto a canvas.
-        // The colored part image will be drawn semi-transparent, with an opacity of
-        // 0.7, allowing for the original image to be visible under.
-        bodyPix.drawMask(
-          canvas,
-          video,
-          coloredPartImage,
-          opacity,
-          maskBlurAmount,
-          flipHorizontal
-        );
-      }
-    }
+async function perform(net) {
+  while (1) {
+    const segmentation = await net.segmentPerson(video);
+    const coloredPartImage = bodyPix.toMask(segmentation);
+    const opacity = 1;
+    const flipHorizontal = false;
+    const maskBlurAmount = 0;
+    // Draw the mask image on top of the original image onto a canvas.
+    // The colored part image will be drawn semi-transparent, with an opacity of
+    // 0.7, allowing for the original image to be visible under.
+    bodyPix.drawMask(
+      canvas,
+      video,
+      coloredPartImage,
+      opacity,
+      maskBlurAmount,
+      flipHorizontal
+    );
+  }
+}
--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
 function postHex(joinB) {
   //console.log(joinB);
-  const BaseUrl = "https://data.youmayalsolike.ml/"; //http://127.0.0.1:5000  //https://data.youmayalsolike.ml/
+  const BaseUrl = "https://data.youmayalsolike.work/"; //http://127.0.0.1:5000  //https://data.youmayalsolike.ml/
   const cameraPalette = `${joinB}`;
   const PaletteUrl = `${BaseUrl}/palette/${cameraPalette}?num=ˇ`;
 
@@ -8,6 +8,11 @@ function postHex(joinB) {
   const showImage = (imgURLs, targetElement) => {
     targetElement.style.backgroundImage = `url(${imgURLs})`;
   };
+  /** show image **/
+  // const showMyImage = (imgURLs, myTargetElement) => {
+  //   myTargetElement.style.backgroundImage = `url(${imgURLs})`;
+  //   // myTargetElement.style.background = `#fff`;
+  // };
 
   /** create palette divs **/
   const createColorElement = (hex) => {
@@ -27,6 +32,7 @@ function postHex(joinB) {
       return;
     }
     const dataPaletteElements = document.querySelectorAll("div.dataPalette");
+    // const myDataPaletteElements = document.getElementById("pop1_img");
     const appliedImages = images.slice(0, dataPaletteElements.length);
     appliedImages.forEach(
       /** @param {{file_name: string, palette: string, url: string}} img */ (
@@ -34,6 +40,7 @@ function postHex(joinB) {
         idx
       ) => {
         const targetElement = dataPaletteElements[idx];
+        // const myTargetElement = myDataPaletteElements;
         const colorElements = img.palette
           .split("-")
           .map((hex) => createColorElement(hex));
@@ -43,12 +50,14 @@ function postHex(joinB) {
         newBlock.className = "nbox";*/
         /** clear palette divs **/
         targetElement.innerHTML = "";
+        // myTargetElement.innerHTML = "";
         /** draw the new hex in to palette divs **/
         colorElements.forEach((ele) => targetElement.appendChild(ele));
 
         /** get images url **/
         const imgURLs = img.url;
         showImage(imgURLs, targetElement);
+        // showMyImage(imgURLs, myTargetElement);
       }
     );
   };
